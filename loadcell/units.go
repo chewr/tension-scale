6033cfa71c2719f614dad6d61a40d5a3f681fdaf
Richard Chew <rchew@palantir.com>
2020-04-18 16:13:51 -0700

    float64 -> float32

--- loadcell/units.go
+++ loadcell/units.go
@@ -1,20 +1,20 @@
 package loadcell
 
-type Newtons float64
-type Pounds float64
-type Kilograms float64
+type Newtons float32
+type Pounds float32
+type Kilograms float32
 
 const (
-	newtonsPerKilogram float64 = 9.799   // m/s^2 (gravitational acceleration)
-	poundsPerKilogram  float64 = 2.20462 // lb/kg (defined unitless constant)
-	newtonsPerPound    float64 = 4.4448  // n/lb  (gravitational acceleration, in weird units)
+	newtonsPerKilogram float32 = 9.799   // m/s^2 (gravitational acceleration)
+	poundsPerKilogram  float32 = 2.20462 // lb/kg (defined unitless constant)
+	newtonsPerPound    float32 = 4.4448  // n/lb  (gravitational acceleration, in weird units)
 )
 
-func NewtonsFromKilograms(kg Kilograms) Newtons { return Newtons(float64(kg) * newtonsPerKilogram) }
-func NewtonsFromPounds(lb Pounds) Newtons       { return Newtons(float64(lb) * newtonsPerPound) }
+func NewtonsFromKilograms(kg Kilograms) Newtons { return Newtons(float32(kg) * newtonsPerKilogram) }
+func NewtonsFromPounds(lb Pounds) Newtons       { return Newtons(float32(lb) * newtonsPerPound) }
 
-func KilogramsFromPounds(lb Pounds) Kilograms  { return Kilograms(float64(lb) / poundsPerKilogram) }
-func KilogramsFromNewtons(n Newtons) Kilograms { return Kilograms(float64(n) / newtonsPerKilogram) }
+func KilogramsFromPounds(lb Pounds) Kilograms  { return Kilograms(float32(lb) / poundsPerKilogram) }
+func KilogramsFromNewtons(n Newtons) Kilograms { return Kilograms(float32(n) / newtonsPerKilogram) }
 
-func PoundsFromKilograms(kg Kilograms) Pounds { return Pounds(float64(kg) * poundsPerKilogram) }
-func PoundsFromNewtons(n Newtons) Pounds      { return Pounds(float64(n) / newtonsPerPound) }
+func PoundsFromKilograms(kg Kilograms) Pounds { return Pounds(float32(kg) * poundsPerKilogram) }
+func PoundsFromNewtons(n Newtons) Pounds      { return Pounds(float32(n) / newtonsPerPound) }
