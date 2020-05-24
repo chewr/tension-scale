0b6784feff841ab3afd3c1374f00040f6b883a80
Richard Chew <rchew@palantir.com>
2020-05-23 18:03:35 -0600

    Add spinner test command

--- cmd/hangboard/internal/cmd/dev/dev.go
+++ cmd/hangboard/internal/cmd/dev/dev.go
@@ -1,16 +1,20 @@
 package dev
 
 import (
-	"github.com/chewr/tension-scale/cmd/hangboard/internal/wip"
+	"github.com/chewr/tension-scale/cmd/hangboard/internal/cmd/dev/spintest"
 	"github.com/spf13/cobra"
 )
 
 var devCmd = &cobra.Command{
 	Use:   "dev",
 	Short: "Dev tools",
-	RunE:  func(cmd *cobra.Command, args []string) error { return wip.ErrTODO },
 }
 
 func AddCommands(rootCmd *cobra.Command) {
+	setup(devCmd)
 	rootCmd.AddCommand(devCmd)
+}
+
+func setup(devCmd *cobra.Command) {
+	spintest.AddCommands(devCmd)
 }
---
+++ cmd/hangboard/internal/cmd/dev/spintest/spintest.go
@@ -0,0 +1,53 @@
+package spintest
+
+import (
+	"time"
+
+	"github.com/briandowns/spinner"
+	"github.com/spf13/cobra"
+)
+
+var spinnerCmd = &cobra.Command{
+	Use:   "spinner",
+	Short: "test the spinner",
+	RunE:  doSpinner,
+}
+
+func AddCommands(rootCmd *cobra.Command) {
+	addFlags(spinnerCmd)
+	rootCmd.AddCommand(spinnerCmd)
+}
+
+const (
+	flagSuffix         = "suffix"
+	flagDefaultCharset = "default-charset"
+)
+
+func addFlags(cmd *cobra.Command) {
+	cmd.Flags().StringP(flagSuffix, "s", "", "spinner suffix")
+	cmd.Flags().IntP(flagDefaultCharset, "d", 0, "default charset")
+}
+
+func doSpinner(cmd *cobra.Command, args []string) error {
+	var opts []spinner.Option
+	opts = append(opts, spinner.WithWriter(cmd.OutOrStdout()))
+
+	suffix, err := cmd.Flags().GetString(flagSuffix)
+	if err != nil {
+		return err
+	}
+	if suffix != "" {
+		opts = append(opts, spinner.WithSuffix(suffix))
+	}
+
+	charSetIndex, err := cmd.Flags().GetInt(flagDefaultCharset)
+	if err != nil {
+		return err
+	}
+
+	s := spinner.New(spinner.CharSets[charSetIndex], 300*time.Millisecond, opts...)
+	s.Start()
+
+	<-cmd.Context().Done()
+	return cmd.Context().Err()
+}
